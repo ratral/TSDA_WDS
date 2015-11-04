cf8205ae121ed2a163bd2b5f7ab042fd4ea7e4e1
Dr. Raul Trujillo Alvarez <dr.ing.trujillo@gmail.com>
2015-11-04 20:16:46 +0100

    Create func_plot_time_serie.R

---
+++ func_plot_time_serie.R
@@ -0,0 +1,33 @@
+#===============================================================================
+#
+#------------------------------------------------------------------------------- 
+plot_time_serie<-function(x,xtyp="days"){
+  
+  x    <- box_plot_data_ts(x,xtyp)
+  graf <- ggplot(x,aes(x=time,y=median))
+  
+  if(xtyp=="days"){graf<-graf+
+    geom_errorbar(colour="gray",size=1.0,aes(ymin=min,ymax=max))+
+    geom_errorbar(colour="blue",size=1.0,aes(ymin=quant.25,ymax=quant.75))+
+    geom_point(colour="red",size=3)}
+  
+  
+  if(xtyp=="hours"){graf<-graf+
+    geom_errorbar(colour="gray",size=1.0,aes(ymin=min,ymax=max))+
+    geom_errorbar(colour="blue",size=1.0,aes(ymin=quant.25,ymax=quant.75))+
+    geom_point(colour="red",size=3)+
+    scale_x_datetime(breaks = date_breaks("2 hour"),
+                     minor_breaks = date_breaks("1 hour"),
+                     labels = date_format("%H:%M"))}
+  
+  if(xtyp=="mins"){graf<-graf+
+    geom_errorbar(colour="gray",size=0.5,aes(ymin=min,ymax=max))+
+    geom_errorbar(colour="blue",size=0.5,aes(ymin=quant.25,ymax=quant.75))+
+    geom_point(colour="red",size=1.5)+
+    scale_x_datetime(breaks = date_breaks("2 hour"),
+                     minor_breaks = date_breaks("1 hour"),
+                     labels = date_format("%H:%M"))}
+  graf
+}
+
+#===============================================================================
