0ee93f1da3ecb1bd21fbccba92e716c94eb28329
Dr. Raul Trujillo Alvarez <dr.ing.trujillo@gmail.com>
2015-11-17 18:16:28 +0100

    Update func_read_time_series.R

--- func_read_time_series.R
+++ func_read_time_series.R
@@ -1,9 +1,25 @@
-read_time_series <- function(file , sep = "," , time_format = "%H:%M:%S" , date_format="'%d.%m.%Y" , value="" , units="" , ...){
-  # Loading Packages
+
+read_time_series <- function(data_file,
+                             date_format= "%H:%M:%S",
+                             time_format ="'%d.%m.%Y" , 
+                             time_zone,sep = ",",
+                             header = TRUE,...){
+  ## Loading Packages
   require(zoo,chron)
   
-  f1 <- function(d, t) as.POSIXct(paste(d, t), format = paste(date_format, time_format))
+  data_serie <- read.csv(file = data_file)
   
-  z1 <- read.zoo(file,header = FALSE,index = 1:2,FUN = f1)
+  ## checked for duplicated
+  data_serie <- unique(data_serie)
+  
+  date_time <- as.POSIXct(paste(data_serie$Date,data_serie$Time),
+                         format = paste(date_format,time_format),
+                         tz = time_zone)
+  
+  data_serie <- data.frame(date_time = date_time,
+                           value = data_serie[,3])
+  
+  data_serie <- read.zoo(data_serie)
+  data_serie <- round_time(data_serie)
   
 }
